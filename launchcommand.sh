242af8f30cca26cf1dadeeb7d9068eb04fa9499c
Andrew Christofi <andrew@intellectualplay.space>
2024-06-09 13:56:31 +0000

    refactored server.properties for kafka

--- launchcommand.sh
+++ launchcommand.sh
@@ -3,12 +3,12 @@
 # Get the machine's IP address
 machine_ip=$(hostname -I | cut -d' ' -f1)
 
-# Path to the server.properties file
-server_properties_file="server.properties"
+# Path to the kafka-server.properties file
+kafka_server_properties_file="kafka-server.properties"
 
 # Update the advertised.listeners property with the machine's IP address
-sed -i "s/advertised.listeners=PLAINTEXT:\/\/localhost:9092/advertised.listeners=PLAINTEXT:\/\/${machine_ip}:9092/" "$PWD/$server_properties_file"
+sed -i "s/advertised.listeners=PLAINTEXT:\/\/localhost:9092/advertised.listeners=PLAINTEXT:\/\/${machine_ip}:9092/" "$PWD/$kafka_server_properties_file"
 
-echo "Updated server.properties file with machine's IP address: $machine_ip"
+echo "Updated kafka-server.properties file with machine's IP address: $machine_ip"
 
 docker-compose --env-file config.env up -d --build
